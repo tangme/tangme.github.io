d366adb24cd41cc789038a43c336aef77a659d00
tangme <tangchaofalv@163.com>
2017-12-18 22:41:23 +0800

    移动版文件初次提交

---
+++ js/mobileIndex.js
@@ -0,0 +1,62 @@
+(function(window) {
+    function PageObj() {
+        this.$selfinfoTab;
+        this.$selfinfoMainDiv;
+        this.$projectTab;
+        this.$projectMainDiv;
+    }
+    PageObj.prototype = {
+        constructor: PageObj
+    }
+    window.PageObj = new PageObj();
+})(window);
+
+$.fn.extend({
+    animateCss: function(animationName, callback) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+            if (callback) {
+                callback();
+            }
+        });
+        return this;
+    }
+});
+
+$(document).ready(function() {
+    PageObj.$selfinfoTab = $("#selfinfo-tab");
+    PageObj.$selfinfoMainDiv = $("#selfinfoMainDiv");
+    PageObj.$projectTab = $("#project-tab");
+    PageObj.$projectMainDiv = $("#projectMainDiv");
+
+
+    PageObj.$projectTab.click(function() {
+        if (!$(this).hasClass("selectedTab")) {
+            $(this).addClass("selectedTab");
+            PageObj.$selfinfoTab.removeClass("selectedTab");
+            PageObj.$selfinfoMainDiv.animateCss("fadeOutRight", function() {
+                PageObj.$selfinfoMainDiv.css("display", "none");
+                PageObj.$projectMainDiv.css("display", "block");
+                PageObj.$projectMainDiv.animateCss("fadeInLeft");
+            });
+        }
+
+    });
+    PageObj.$selfinfoTab.click(function() {
+        if (!$(this).hasClass("selectedTab")) {
+            $(this).addClass("selectedTab");
+            PageObj.$projectTab.removeClass("selectedTab");
+            PageObj.$projectMainDiv.animateCss("fadeOutLeft", function() {
+                PageObj.$projectMainDiv.css("display", "none");
+                PageObj.$selfinfoMainDiv.css("display", "block");
+                PageObj.$selfinfoMainDiv.animateCss("fadeInRight");
+            });
+        }
+
+    });
+    $("#guideTab").sticky({
+            topSpacing: 0,
+            zIndex:9
+    });
+});
