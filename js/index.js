3fcebcbc91a38d7526d744ecb69e65dbe368e5d3
tangme <tangchaofalv@163.com>
2017-12-20 10:12:31 +0800

    js文件压缩引入调整

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-	$(".panel-tab-ul li[data-tab]").click(function(){
+	/*$(".panel-tab-ul li[data-tab]").click(function(){
 		$(".tabContent").css("display","none");
 		$("."+$(this).data("tab")).fadeIn("slow");
-	});
-
+	});*/
+	
+	/*页面初次显示动画*/
 	new WOW().init();
 });
--- js/mobileIndex.js
+++ js/mobileIndex.js
@@ -7,7 +7,7 @@
     }
     PageObj.prototype = {
         constructor: PageObj
-    }
+    };
     window.PageObj = new PageObj();
 })(window);
 
@@ -30,13 +30,13 @@ $(document).ready(function() {
     PageObj.$projectTab = $("#project-tab");
     PageObj.$projectMainDiv = $("#projectMainDiv");
 
-
+    /*项目信息 选项卡点击事件初始化*/
     PageObj.$projectTab.click(function() {
         if (!$(this).hasClass("selectedTab")) {
             $(this).addClass("selectedTab");
             PageObj.$selfinfoTab.removeClass("selectedTab");
-            PageObj.$selfinfoMainDiv.stop(true,true);
-            PageObj.$projectMainDiv.stop(true,true);
+            PageObj.$selfinfoMainDiv.stop(true, true);
+            PageObj.$projectMainDiv.stop(true, true);
             PageObj.$selfinfoMainDiv.animateCss("fadeOutRight", function() {
                 PageObj.$selfinfoMainDiv.css("display", "none");
                 PageObj.$projectMainDiv.fadeIn("slow");
@@ -44,12 +44,13 @@ $(document).ready(function() {
         }
 
     });
+    /*个人信息 选项卡点击事件初始化*/
     PageObj.$selfinfoTab.click(function() {
         if (!$(this).hasClass("selectedTab")) {
             $(this).addClass("selectedTab");
             PageObj.$projectTab.removeClass("selectedTab");
-            PageObj.$selfinfoMainDiv.stop(true,true);
-            PageObj.$projectMainDiv.stop(true,true);
+            PageObj.$selfinfoMainDiv.stop(true, true);
+            PageObj.$projectMainDiv.stop(true, true);
             PageObj.$projectMainDiv.animateCss("fadeOutLeft", function() {
                 PageObj.$projectMainDiv.css("display", "none");
                 PageObj.$selfinfoMainDiv.fadeIn("slow");
@@ -57,9 +58,11 @@ $(document).ready(function() {
         }
 
     });
+    /*固定页面元素初始化*/
     $("#guideTab").sticky({
-            topSpacing: 0,
-            zIndex:9
+        topSpacing: 0,
+        zIndex: 9
     });
+    /*页面动画效果初始化*/
     new WOW().init();
 });
