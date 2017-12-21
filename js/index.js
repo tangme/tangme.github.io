e00024f15ba8afe1fcf762a40f4d550ebc1888b7
tangme <tangchaofalv@163.com>
2017-12-21 17:52:48 +0800

    IE8兼容性改善

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-$(document).ready(function(){
-	/*$(".panel-tab-ul li[data-tab]").click(function(){
-		$(".tabContent").css("display","none");
-		$("."+$(this).data("tab")).fadeIn("slow");
-	});*/
-	
-	/*页面初次显示动画*/
-	new WOW().init();
+$(document).ready(function() {
+    /*$(".panel-tab-ul li[data-tab]").click(function(){
+    	$(".tabContent").css("display","none");
+    	$("."+$(this).data("tab")).fadeIn("slow");
+    });*/
+
+    /*页面初次显示动画*/
+    var imgsArray = $(".my-hobby-ul>li>img");
+    if (!Tools.isOldIE) {
+        new WOW().init();
+        imgsArray.each(function() {
+            $(this).attr("src", "./images/" + $(this).data("imgsrcname") + ".svg");
+        });
+    } else {
+        imgsArray.each(function() {
+            $(this).attr("src", "./images/" + $(this).data("imgsrcname") + ".png");
+        });
+    }
+
 });
