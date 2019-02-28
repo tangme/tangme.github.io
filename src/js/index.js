d7002a4037b9857cef579f092007053b2ffee875
tanglv <tangchaofalv@163.com>
2019-02-28 21:05:19 +0800

    新建vue分支，并初始化内容

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
+let tmp = '';
+var projectApp = new Vue({
+    el:'#tmpApp',
+    created:function(){
+        console.log('i am here.');
+        $.getJSON("./js/datas.json", function(json){
+            console.log('====');
+            projectApp.projectDatas = json;
+            console.log(json);
+            tmp = json;
+        });
+    },
+    data:{
+        projectDatas:[]
+    }
+});
+
+console.log('lalalal');
+
 $(document).ready(function() {
     /*$(".panel-tab-ul li[data-tab]").click(function(){
     	$(".tabContent").css("display","none");
